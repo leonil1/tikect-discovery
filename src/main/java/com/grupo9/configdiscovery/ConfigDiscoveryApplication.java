b9ca1feb0458b9650be77cfc0785f57d4996ead0
leonil10 <chipana.leo1@gmail.com>
2024-03-26 11:38:10 -0500

    structura de erureka  dicovery

---
+++ src/main/java/com/grupo9/configdiscovery/ConfigDiscoveryApplication.java
@@ -0,0 +1,13 @@
+package com.grupo9.configdiscovery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigDiscoveryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigDiscoveryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/grupo9/configdiscovery/ConfigDiscoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grupo9.configdiscovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigDiscoveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
